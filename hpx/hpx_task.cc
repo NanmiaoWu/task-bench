267d62cf41342d15cc3e233e662ec153b3acdf19
nanmiaowu <nanmiaowu@gmail.com>
2021-08-02 21:48:56 -0500

    update hpx_implementation: miss three parts

--- hpx/hpx_task.cc
+++ hpx/hpx_task.cc
@@ -13,18 +13,18 @@
 int hpx_main(int argc, char *argv[]) 
 { 
 
-  // get number of threads and this thread
-  std::uint32_t num_threads = hpx::get_num_localities(hpx::launch::sync);
-  std::uint32_t this_thread = hpx::get_locality_id();
+  // get number of localities and this locality
+  std::uint32_t num_localities = hpx::get_num_localities(hpx::launch::sync);
+  std::uint32_t this_locality = hpx::get_locality_id();
 
   App app(argc, argv);
-  if (this_thread == 0) app.display();
+  if (this_locality == 0) app.display();
 
   std::vector<std::vector<char> > scratch;
 
   for (auto graph : app.graphs) {
-    long first_point = this_thread * graph.max_width / num_threads;
-    long last_point = (this_thread + 1) * graph.max_width / num_threads - 1;
+    long first_point = this_locality * graph.max_width / num_localities;
+    long last_point = (this_locality + 1) * graph.max_width / num_localities - 1;
     long n_points = last_point - first_point + 1;
 
     size_t scratch_bytes = graph.scratch_bytes_per_task;
@@ -32,24 +32,23 @@ int hpx_main(int argc, char *argv[])
     TaskGraph::prepare_scratch(scratch.back().data(), scratch.back().size());
   }
 
-  
   for (int iter = 0; iter < 2; ++iter) {
     hpx::chrono::high_resolution_timer timer;
 
     for (auto graph : app.graphs) {
-      long first_point = this_thread * graph.max_width / n_threads;
-      long last_point = (this_thread + 1) * graph.max_width / n_threads - 1;
+      long first_point = this_locality * graph.max_width / n_threads;
+      long last_point = (this_locality + 1) * graph.max_width / n_threads - 1;
       long n_points = last_point - first_point + 1;
 
       size_t scratch_bytes = graph.scratch_bytes_per_task;
       char *scratch_ptr = scratch[graph.graph_index].data();
 
-      std::vector<int> thread_by_point(graph.max_width);
-      for (int r = 0; r < num_threads; ++r) {
-        long r_first_point = r * graph.max_width / num_threads;
-        long r_last_point = (r + 1) * graph.max_width / num_threads - 1;
+      std::vector<int> locality_by_point(graph.max_width);
+      for (int r = 0; r < num_localities; ++r) {
+        long r_first_point = r * graph.max_width / num_localities;
+        long r_last_point = (r + 1) * graph.max_width / num_localities - 1;
         for (long p = r_first_point; p <= r_last_point; ++p) {
-          thread_by_point[p] = r;
+          locality_by_point[p] = r;
         }
       }
 
@@ -105,10 +104,7 @@ int hpx_main(int argc, char *argv[])
           reverse_dependencies[dset][point_index] = graph.reverse_dependencies(dset, point);
         }
       }
-
-      // begins 
-      std::vector<std::vector<hpx::lcos::future<void>>> futures;
-
+  
       for (long timestep = 0; timestep < graph.timesteps; ++timestep) {
         long offset = graph.offset_at_timestep(timestep);
         long width = graph.width_at_timestep(timestep);
@@ -120,8 +116,6 @@ int hpx_main(int argc, char *argv[])
         auto &deps = dependencies[dset];
         auto &rev_deps = reverse_dependencies[dset];
 
-        std::vector<hpx::lcos::future<void>> future_vec;
-
         for (long point = first_point; point <= last_point; ++point) {
           long point_index = point - first_point;
 
@@ -132,27 +126,78 @@ int hpx_main(int argc, char *argv[])
           auto &point_deps = deps[point_index];
           auto &point_rev_deps = rev_deps[point_index];
 
+          std::vector<hpx::lcos::future<std::vector<char>>> futures;
+
+          /* Receive */
+          point_n_inputs = 0;
           if (point >= offset && point < offset + width) {
-              
+            for (auto interval : point_deps) {
+              for (long dep = interval.first; dep <= interval.second; ++dep) {
+                if (dep < last_offset || dep >= last_offset + last_width) {
+                  continue;
+                }
+
+                // Use shared memory for on-node data.
+                if (first_point <= dep && dep <= last_point) {
+                  auto &output = outputs[dep - first_point];
+                  point_inputs[point_n_inputs].assign(output.begin(), output.end());
+                } else {
+
+                  // fix me
+                  hpx::lcos::future<std::vector<char>> result = hpx::async();
+                  MPI_Irecv(point_inputs[point_n_inputs].data(),
+                            point_inputs[point_n_inputs].size(), MPI_BYTE,
+                            rank_by_point[dep], tag, MPI_COMM_WORLD, &req);
+                  futures.push_back(result);
+                  // end fix me
+                }
+                point_n_inputs++;
+              }
+            }
           }
 
+          /* Send */
+          if (point >= last_offset && point < last_offset + last_width) {
+            for (auto interval : point_rev_deps) {
+              for (long dep = interval.first; dep <= interval.second; dep++) {
+                if (dep < offset || dep >= offset + width || (first_point <= dep && dep <= last_point)) {
+                  continue;
+                }
 
+                // fix me
+                hpx::lcos::future<std::vector<char>> result = hpx::async();
+                MPI_Isend(point_output.data(), point_output.size(), MPI_BYTE,
+                          rank_by_point[dep], tag, MPI_COMM_WORLD, &req);
+                futures.push_back(result);
+                // end fix me
+              }
+            }
+          }
         }
+        // fix me
+        MPI_Waitall(requests.size(), requests.data(), MPI_STATUSES_IGNORE);
+        // end fix me
 
-        futures.push_back(future_vec);
-      
-      }
+        for (long point = std::max(first_point, offset); point <= std::min(last_point, offset + width - 1); ++point) {
+          long point_index = point - first_point;
 
+          auto &point_input_ptr = input_ptr[point_index];
+          auto &point_input_bytes = input_bytes[point_index];
+          auto &point_n_inputs = n_inputs[point_index];
+          auto &point_output = outputs[point_index];
 
-    
+          graph.execute_point(timestep, point,
+                              point_output.data(), point_output.size(),
+                              point_input_ptr.data(), point_input_bytes.data(), point_n_inputs,
+                              scratch_ptr + scratch_bytes * point_index, scratch_bytes);
+        }      
+      }    
     }
-
-    
-    
+  
     double elapsed = timer.elapsed(); 
   }
 
-  if (this_thread == 0) {
+  if (this_locality == 0) {
     app.report_timing(elapsed);
   }
 
